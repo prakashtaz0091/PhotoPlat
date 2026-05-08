9c75a90b1282e322fad3e8fc710888025554f822
prakashtaz0091 <code.ing2468@gmail.com>
2026-05-08 22:01:34 +0545

    docker setup

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -2,7 +2,7 @@ from . base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -38,3 +38,15 @@ MIDDLEWARE = [
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST", "db"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
--- project/wsgi.py
+++ project/wsgi.py
@@ -5,10 +5,8 @@ from dotenv import load_dotenv
 DJANGO_ENV = os.environ.get("DJANGO_ENV")
 
 if DJANGO_ENV == "prod":
-    load_dotenv(".env.production")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.prod")
 else:
-    load_dotenv(".env.local")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.local")
     
 
